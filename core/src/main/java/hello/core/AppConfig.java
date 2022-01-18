99b3608a8b29c13cd3e272f00e999135dedd8e27
dalsa <dalsa@jaranda.kr>
2022-01-18 11:09:30 +0900

    AppConfig 를 리펙토링해서 제대로 분리시키는 부분

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,7 @@
 package hello.core;
 
-import hello.core.dicount.FixDiscountPolicy;
+import hello.core.dicount.DiscountPolicy;
+import hello.core.dicount.RateDicountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +11,18 @@ import hello.core.order.OrderserviceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자를 통해서 들어간다고 해서 생성자 주입이라고 한다.
+        return new MemberServiceImpl(memberRepository()); // 생성자를 통해서 들어간다고 해서 생성자 주입이라고 한다.
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderserviceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderserviceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDicountPolicy();
     }
 }
